f3df26545e8e4a2925fed0124b3f1848957ec83a
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2018-01-10 23:19:51 -0200

    Allowing .. in relativeRequire

--- lib/utils.lua
+++ lib/utils.lua
@@ -1,13 +1,47 @@
 
 local Utils = {}
+local Private = {}
+
+
+
+-- Private Methods
+
+function Private.processPath(pathString)
+    local goBacks = 0
+
+    local it = pathString:gmatch("%.%./")
+    while it() do
+        goBacks = goBacks + 1
+    end
+
+    local path = pathString:match("[^%.%./].*")
+    return goBacks, path
+end
+
+
+function Private.processFolder(folder, goBacks)
+    for _ = 1, goBacks do
+        folder = folder:gsub("[^%.]+%.?$", "")
+    end
+
+    return folder
+end
+
+
+
+-- Public Methods
 
 function Utils.setUtils()
     --stackoverflow:
     --https://stackoverflow.com/questions/9145432/load-lua-files-by-relative-path
     function relativeRequire(path, ...)
         local pathOfThisFile = ...
+
+        local goBacks, processedPath = Private.processPath(path)
         local folderOfThisFile = pathOfThisFile:match("(.-)[^%.]+$")
-        return require(folderOfThisFile .. path)
+
+        local folder = Private.processFolder(folderOfThisFile, goBacks)
+        return require(folder .. processedPath)
     end
 end
 
